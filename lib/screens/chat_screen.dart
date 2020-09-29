0bd1e6ed287e655b622a6e9312c7d06ca07a1b32
diehard04 <dipakmehta0910@gmail.com>
2020-09-29 12:59:56 +0530

    firebase added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold (
-     body: ListView.builder(
-      itemCount: 10,
-        itemBuilder: (context, index) => Container(
-              padding: EdgeInsets.all(8.0),
-              child: Text('This Works'),
-            ))
+    return Scaffold(
+      body: StreamBuilder(
+          stream: Firestore.instance
+              .collection("chats/hxQHP2ZQ3bQx1RzAOKnt/messages")
+              .snapshots(),
+          builder: (ctx, streamSnapShot) {
+            if(streamSnapShot.connectionState ==ConnectionState.waiting) {
+               return Center(
+                 child: CircularProgressIndicator(),
+               );
+            }
+            final documents = streamSnapShot.data.documents;
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (context, index) => Container(
+                padding: EdgeInsets.all(8.0),
+                child: Text(documents[index]['text']),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+
+        },
+      ),
     );
   }
 }
