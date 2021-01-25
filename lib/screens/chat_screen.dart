2a815b88e86112fa10004484cd013cce63733bea
DIPAK KUMAR MEHTA <dipakmehta0910@gmail.com>
2021-01-25 21:17:28 +0530

    push notif

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,8 +4,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cheetoo/widgets/chat/messages.dart';
 import 'package:cheetoo/widgets/chat/new_message.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatScreen extends StatelessWidget {
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
